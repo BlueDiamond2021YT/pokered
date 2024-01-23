439436f3a21ab330037faccc70f91ec5b022b268
BlueDiamond2021YT <139054327+BlueDiamond2021YT@users.noreply.github.com>
2024-01-23 20:15:34 +0000

    modified:   data/pokemon/names.asm

--- data/pokemon/names.asm
+++ data/pokemon/names.asm
@@ -30,8 +30,8 @@ MonsterNames::
 	db "BLASTOISE@"
 	db "PINSIR@@@@"
 	db "TANGELA@@@"
-	db "1F-031-@@@" ; [Gyaoon]             [1F] [031] [---]
-	db "20-032-@@@" ; [NidoranM -like Pkm] [20] [032] [---]
+	db "1F-031-???" ; [Gyaoon]             [1F] [031] [---]
+	db "20-032-???" ; [NidoranM -like Pkm] [20] [032] [---]
 	db "GROWLITHE@"
 	db "ONIX@@@@@@"
 	db "FEAROW@@@@"
@@ -49,26 +49,26 @@ MonsterNames::
 	db "PSYDUCK@@@"
 	db "DROWZEE@@@"
 	db "GOLEM@@@@@"
-	db "32-050-@@@" ; [Balloonda]          [32] [050] [---]
+	db "32-050-???" ; [Balloonda]          [32] [050] [---]
 	db "MAGMAR@@@@"
-	db "34-052-@@@" ; [Buu]                [34] [052] [---]
+	db "34-052-???" ; [Buu]                [34] [052] [---]
 	db "ELECTABUZZ"
 	db "MAGNETON@@"
 	db "KOFFING@@@"
-	db "38-056-@@@" ; [Deer]               [38] [056] [---]
+	db "38-056-???" ; [Deer]               [38] [056] [---]
 	db "MANKEY@@@@"
 	db "SEEL@@@@@@"
 	db "DIGLETT@@@"
 	db "TAUROS@@@@"
-	db "3D-061-@@@" ; [Elephant Pkm]       [3D] [061] [---]
-	db "3E-062-@@@" ; [Crocky]             [3E] [062] [---]
-	db "3F-063-@@@" ; [Squid Pkm 1]        [3F] [063] [---]
+	db "3D-061-???" ; [Elephant Pkm]       [3D] [061] [---]
+	db "3E-062-???" ; [Crocky]             [3E] [062] [---]
+	db "3F-063-???" ; [Squid Pkm 1]        [3F] [063] [---]
 	db "FARFETCH'D"
 	db "VENONAT@@@"
 	db "DRAGONITE@"
-	db "43-067-@@@" ; [Cactus]             [43] [067] [---]
-	db "44-068-@@@" ; [Jaggu]              [44] [068] [---]
-	db "35-069-@@@" ; [Zulbat pre-evo]     [45] [069] [---]
+	db "43-067-???" ; [Cactus]             [43] [067] [---]
+	db "44-068-???" ; [Jaggu]              [44] [068] [---]
+	db "35-069-???" ; [Zulbat pre-evo]     [45] [069] [---]
 	db "DODUO@@@@@"
 	db "POLIWAG@@@"
 	db "JYNX@@@@@@"
@@ -78,23 +78,23 @@ MonsterNames::
 	db "DITTO@@@@@"
 	db "MEOWTH@@@@"
 	db "KRABBY@@@@"
-	db "4F-079-@@@" ; [Fish Pkm 1]         [4F] [079] [---]
-	db "50-080-@@@" ; [Fish Pkm 2]         [50] [080] [---]
-	db "51-081-@@@" ; [Vulpix pre-evo]     [51] [081] [---]
+	db "4F-079-???" ; [Fish Pkm 1]         [4F] [079] [---]
+	db "50-080-???" ; [Fish Pkm 2]         [50] [080] [---]
+	db "51-081-???" ; [Vulpix pre-evo]     [51] [081] [---]
 	db "VULPIX@@@@"
 	db "NINETALES@"
 	db "PIKACHU@@@"
 	db "RAICHU@@@@"
-	db "56-086-@@@" ; [Frog-like pkm 1]    [56] [086] [---]
-	db "57-087-@@@" ; [Frog-like Pkm 2]    [57] [087] [---]
+	db "56-086-???" ; [Frog-like pkm 1]    [56] [086] [---]
+	db "57-087-???" ; [Frog-like Pkm 2]    [57] [087] [---]
 	db "DRATINI@@@"
 	db "DRAGONAIR@"
 	db "KABUTO@@@@"
 	db "KABUTOPS@@"
 	db "HORSEA@@@@"
 	db "SEADRA@@@@"
-	db "5E-094-@@@" ; [Lizard Pkm 2]       [5E] [094] [---]
-	db "5F-095-@@@" ; [Lizard Pkm 3]       [5F] [095] [---]
+	db "5E-094-???" ; [Lizard Pkm 2]       [5E] [094] [---]
+	db "5F-095-???" ; [Lizard Pkm 3]       [5F] [095] [---]
 	db "SANDSHREW@"
 	db "SANDSLASH@"
 	db "OMANYTE@@@"
@@ -114,38 +114,38 @@ MonsterNames::
 	db "WEEDLE@@@@"
 	db "KAKUNA@@@@"
 	db "BEEDRILL@@"
-	db "73-115-@@@" ; [Unknown]            [73] [115] [---]
+	db "73-115-???" ; [Unknown]            [73] [115] [---]
 	db "DODRIO@@@@"
 	db "PRIMEAPE@@"
 	db "DUGTRIO@@@"
 	db "VENOMOTH@@"
 	db "DEWGONG@@@"
-	db "79-121-@@@" ; [Unknown]            [79] [121] [---]
-	db "7A-122-@@@" ; [Squid Pkm 2]        [7A] [122] [---]
+	db "79-121-???" ; [Unknown]            [79] [121] [---]
+	db "7A-122-???" ; [Squid Pkm 2]        [7A] [122] [---]
 	db "CATERPIE@@"
 	db "METAPOD@@@"
 	db "BUTTERFREE"
 	db "MACHAMP@@@"
-	db "7F-127-@@@" ; [Golduck mid-evo]    [7F] [127] [---]
+	db "7F-127-???" ; [Golduck mid-evo]    [7F] [127] [---]
 	db "GOLDUCK@@@"
 	db "HYPNO@@@@@"
 	db "GOLBAT@@@@"
 	db "MEWTWO@@@@"
 	db "SNORLAX@@@"
 	db "MAGIKARP@@"
-	db "86-134-@@@" ; [Meowth pre-evo]     [86] [134] [---]
-	db "87-135-@@@" ; [Unknown]            [87] [135] [---]
+	db "86-134-???" ; [Meowth pre-evo]     [86] [134] [---]
+	db "87-135-???" ; [Unknown]            [87] [135] [---]
 	db "MUK@@@@@@@"
-	db "89-137-@@@" ; [Gyaoon pre-evo]     [89] [137] [---]
+	db "89-137-???" ; [Gyaoon pre-evo]     [89] [137] [---]
 	db "KINGLER@@@"
 	db "CLOYSTER@@"
-	db "92-140-@@@" ; [Magneton-like pkm]  [8C] [140] [---]
+	db "92-140-???" ; [Magneton-like pkm]  [8C] [140] [---]
 	db "ELECTRODE@"
 	db "CLEFABLE@@"
 	db "WEEZING@@@"
 	db "PERSIAN@@@"
 	db "MAROWAK@@@"
-	db "92-146-@@@" ; [Marowak evo]        [92] [146] [---]
+	db "92-146-???" ; [Marowak evo]        [92] [146] [---]
 	db "HAUNTER@@@"
 	db "ABRA@@@@@@"
 	db "ALAKAZAM@@"
@@ -155,13 +155,13 @@ MonsterNames::
 	db "BULBASAUR@"
 	db "VENUSAUR@@"
 	db "TENTACRUEL"
-	db "9C-156-@@@" ; [Goldeen pre-evo]    [9C] [156] [---]
+	db "9C-156-???" ; [Goldeen pre-evo]    [9C] [156] [---]
 	db "GOLDEEN@@@"
 	db "SEAKING@@@"
-	db "9F-159-@@@" ; [Kotora]             [9F] [159] [---]
-	db "A0-160-@@@" ; [Raitora]            [A0] [160] [---]
-	db "A1-161-@@@" ; [Raitora evo]        [A1] [161] [---] 
-	db "A2-163-@@@" ; [Ponyta pre-evo]     [A2] [162] [---]
+	db "9F-159-???" ; [Kotora]             [9F] [159] [---]
+	db "A0-160-???" ; [Raitora]            [A0] [160] [---]
+	db "A1-161-???" ; [Raitora evo]        [A1] [161] [---] 
+	db "A2-163-???" ; [Ponyta pre-evo]     [A2] [162] [---]
 	db "PONYTA@@@@"
 	db "RAPIDASH@@"
 	db "RATTATA@@@"
@@ -171,19 +171,19 @@ MonsterNames::
 	db "GEODUDE@@@"
 	db "PORYGON@@@"
 	db "AERODACTYL"
-	db "AC-172-@@@" ; [Blastoise-like pkm] [AC] [172] [---]
+	db "AC-172-???" ; [Blastoise-like pkm] [AC] [172] [---]
 	db "MAGNEMITE@"
-	db "AE-174-@@@" ; [Lizard Pkm 1]       [AE] [174] [---]
-	db "AF-175-@@@" ; [Gorochu]            [AF] [175] [---]
+	db "AE-174-???" ; [Lizard Pkm 1]       [AE] [174] [---]
+	db "AF-175-???" ; [Gorochu]            [AF] [175] [---]
 	db "CHARMANDER"
 	db "SQUIRTLE@@"
 	db "CHARMELEON"
 	db "WARTORTLE@"
 	db "CHARIZARD@"
-	db "B5-181-@@@" ; [Orig Wartorte evo]  [B5] [181] [---]
-	db "B6-182-@@@" ; [Kabutops Fossil]    [B6] [182] [---]
-	db "B7-183-@@@" ; [Aerodactyl Fossil]  [B7] [183] [---]
-	db "B8-184-@@@" ; [Pkm tower ghost]    [B8] [184] [---]
+	db "B5-181-???" ; [Orig Wartorte evo]  [B5] [181] [---]
+	db "B6-182-???" ; [Kabutops Fossil]    [B6] [182] [---]
+	db "B7-183-???" ; [Aerodactyl Fossil]  [B7] [183] [---]
+	db "B8-184-???" ; [Pkm tower ghost]    [B8] [184] [---]
 	db "ODDISH@@@@"
 	db "GLOOM@@@@@"
 	db "VILEPLUME@"
